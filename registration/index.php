9372bd9d0b05de799a770c254470aeb542a193be
ShafinSarwar_1521560642 <68143484+Shafin123-nsu@users.noreply.github.com>
2021-01-12 01:24:51 +0600

    Registration
    Index,regform,student reg. form, student information,university reg. form,university information

---
+++ registration/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>499A</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container-fluid" style="margin:0px;padding:0px;height: 100%;">
+  
+  <div id="myCarousel" class="carousel slide" data-ride="carousel">
+    
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active">
+        <img src="img/A.png" alt="Los Angeles" style="width:100%;height: 100%;">
+        
+      </div>
+
+      <div class="item">
+        <img src="img/B.png" alt="Chicago" style="width:100%;height: 100%;">
+        
+      </div>
+    
+      
+  
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+      
+    </a>
+    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+      
+    </a>
+  </div>
+</div>
+
+<div class="container" style="position: fixed;bottom: 415px;left: 235px;right: 0;">
+  <h2 style="color:#EA4D06;">Visit website as a guest</h2>
+  <button type="button" class="btn btn-primary" style="background-color:#EA4D06;border-radius: 0px;">ENTER</button>
+  
+</div>
+<div class="container" style="position: fixed;bottom: 788px;left: 1161px;right: 0;">
+  
+  <button type="button"  class="btn btn-primary"  style="background-color:#EA4D06;border-radius: 19px;">
+    <a  href="regform.php" style="text-decoration:none; color:#FFFFFF;">Registration</a></button>
+  <button type="button" class="btn btn-primary" style="background-color:#EA4D06;border-radius: 19px;">Login</button>
+
+</div>
+</body>
+</html>
